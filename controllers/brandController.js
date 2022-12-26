d3401733abb253eab9a28a39a64022b3c2a3ddb5
Dzmitry Panasiuk <108349711+Dzmitry-Panasiuk@users.noreply.github.com>
2022-12-26 20:12:18 +0100

    add controllers to the project (#1)
    * add controllers to the project
    
    * refactoring

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,10 +1,15 @@
+const {Type, Brand} = require('../models/models');
+const ApiError = require('../error/ApiError');
 class BrandController {
-    async create () {
-
+    async create (req, res) {
+        const {name} = req.body;
+        const brand = await Brand.create({ name });
+        return res.json(brand);
     }
 
-    async getAll() {
-
+    async getAll(req, res) {
+        const brands = await Brand.findAll();
+        return res.json(brands);
     }
 }
 
--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload')
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 8082;
 
 const app = express();
 app.use(cors()); //чтобы можно было посылать запросы с браузера
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler); // последний middleware
 
@@ -23,7 +27,7 @@ const start = async () => {
     app.listen(PORT,  
       () => console.log(`Running on ${PORT}`)
     );
-  } catch {
+  } catch (e) {
     console.log(e);
   }
 }
