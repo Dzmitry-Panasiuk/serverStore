8741f0d284774ee51e8c3e6e7f223bd437bcb95c
Dzmitry Panasiuk <108349711+Dzmitry-Panasiuk@users.noreply.github.com>
2022-12-31 17:05:56 +0100

    Add login to the app (#2)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,55 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require('bcrypt');
+const {User, Basket} = require('../models/models');
+const jwt = require('jsonwebtoken');
+
+const generateJwt = (id, email, role) => {
+   return jwt.sign(
+        { id, email, role }, 
+        process.env.SECRET_KEY,
+        { expiresIn: '24h' }
+    )
+};
 
 class UserController {
-    async registration () {
+    async registration (req, res, next) {
+        const {email , password, role} = req.body;
 
+        if(!email || !password) {
+            return next(ApiError.badRequest('Bad email or password'));
+        }
+        const candidate = await User.findOne({where: {email}});
+        if(candidate) {
+            return next(ApiError.badRequest('User is already exists'))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        const basket = await Basket.create({userId: user.id});
+        const token = generateJwt(user.id, email, user.role);
+
+        return res.json({token});
     }
 
-    async login() {
+    async login(req, res, next) {
+        const {email, password} = req.body;
+        const user = await User.findOne({where: {email}});
+        if(!user) {
+            return next(ApiError.internal("User didn't find"));
+        }
 
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if(!comparePassword) {
+            return next(ApiError.internal("Not right password"));
+        }
+
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({token});
     }
 
     async check (req, res, next) {
-        const {id, ...response} = req.query;
-
-        if(!id) {
-            return next(ApiError.badRequest('Не задан id'));
-        }
-
-        res.json({id, response})
+        const token = generateJwt(req.user.id, req.user.email, req.user.role);
+        return res.json({token});
     }
 }
 
